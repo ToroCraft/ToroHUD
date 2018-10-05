4a2080317c6f460429ad1ee1955aa5446f2afa58
Charles Howard <frodare@gmail.com>
2018-10-05 19:16:54 -0400

    step to v5

--- src/main/java/net/torocraft/torohud/ToroHUD.java
+++ src/main/java/net/torocraft/torohud/ToroHUD.java
@@ -18,7 +18,7 @@ import net.torocraft.torohud.proxy.CommonProxy;
 public class ToroHUD {
 
   public static final String MODID = "torohud";
-  public static final String VERSION = "1.12.2-4";
+  public static final String VERSION = "1.12.2-5";
   public static final String MODNAME = "ToroHUD";
 
   @SidedProxy(clientSide = "net.torocraft.torohud.proxy.ClientProxy", serverSide = "net.torocraft.torohud.proxy.ServerProxy")
